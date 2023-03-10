754cf9b06f3352a6bafcc556aee6a0805c7fdeb4
jmdeaton <126132797+jmdeaton@users.noreply.github.com>
2023-03-10 16:53:03 -0500

    Add files via upload

--- stats.py
+++ stats.py
@@ -8,11 +8,15 @@ def list_min(l):
 def list_max(l): 
     return max(l)
 
-print(list_max([1, 2, 3]))          # This should return 3
-print(list_max(['bb', 'cb', 'abc'])) # This should return 'cb'
+def list_range(l): 
+    return max(l) - min(l) 
 
+def list_mean(l): 
+    return sum(l) / len(l) 
 
-
+def list_median(l): 
+    sorted(l)
+    
 
 
 
